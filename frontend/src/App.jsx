239385d650a54f556be8d161f1e777481b84bb33
sofia santos <sofia.parreira.santos@gmail.com>
2024-11-11 23:52:42 +0000

    nomes variaveis

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Drawing from './pages/mobile/Drawing';
 import EnterRoom from './pages/mobile/EnterRoom';
+import InfoGiver from './components/InfoGiver';
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/drawing" element={<Drawing />} />
         <Route path="/" element={<EnterRoom />} />
+        <Route path="/info" element={<InfoGiver text="ID da Sala" role="Digite o ID da sala"/>} />
       </Routes>
     </Router>
   )
