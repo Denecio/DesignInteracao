2b8fb1f59c5b95474c9b17cf62d2583e97943884
Denecio <vascopereirabastos@gmail.com>
2024-11-11 17:55:35 +0000

    Setup backend and frontend

---
+++ frontend/src/pages/mobile/Drawing/index.jsx
@@ -0,0 +1,15 @@
+// frontend/src/App.js
+
+import React from 'react';
+import Canvas from '../../../components/Canvas';
+
+function Drawing() {
+  return (
+    <div className="App">
+      <h1>Real-Time Drawing App</h1>
+      <Canvas />
+    </div>
+  );
+}
+
+export default Drawing;
