4226ba477e1343a47a11ee74b65eae71fb62e797
sofia santos <sofia.parreira.santos@gmail.com>
2024-11-12 18:31:48 +0000

    components final frames

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,8 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Drawing from './pages/mobile/Drawing';
 import EnterRoom from './pages/mobile/EnterRoom';
-import InfoGiver from './components/infogiver';
+import InfoGiver from './components/InfoGiver';
+import FinalFrames from './components/FinalFrames';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/drawing" element={<Drawing />} />
         <Route path="/" element={<EnterRoom />} />
+        <Route path="/arrangeframes" element={<FinalFrames number="1" />} />
       </Routes>
     </Router>
   )
