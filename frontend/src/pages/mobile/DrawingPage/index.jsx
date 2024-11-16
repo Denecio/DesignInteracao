730bdfed3a557a2096c464d68a21a5fc56f6522e
sofia santos <sofia.parreira.santos@gmail.com>
2024-11-16 00:21:29 +0000

    teta

--- frontend/src/pages/mobile/DrawingPage/index.jsx
+++ frontend/src/pages/mobile/DrawingPage/index.jsx
@@ -4,13 +4,23 @@ import brush from "../../../assets/icons/brush.png"
 import eraser from "../../../assets/icons/eraser.png"
 import trash from "../../../assets/icons/trash.png"
 
-const DrawingPage = () => {
-    return(
-        <div className="enterroom_container">
-            <div className="container">
-                <input type="text" className="input" placeholder="ID da Sala"/>
+const DrawingPage = ({ texto }) => {
+    return (
+        <div className="drawingpage">
+            <div className="drawingpage_container">
+                <p className="arrangetext">{texto}texto da história</p>
+
+
+
             </div>
-            <button className="btn-enter"> <img src={check} alt="Confirm"/> </button>
+
+            <button className="btn-enter"> <img src={check} alt="Confirm" /> </button>
+            <div className="buttons">
+                <button className="btn-brush"> <img src={brush} alt="Confirm" /> </button>
+                <button className="btn-eraser"> <img src={eraser} alt="Confirm" /> </button>
+                <button className="btn-trash"> <img src={trash} alt="Confirm" /> </button>
+            </div>
+
         </div>
     )
 }
