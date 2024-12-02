84d5cb8dc2436c68c9b9bffa71eb45c3fc02e672
Denecio <vascopereirabastos@gmail.com>
2024-12-02 11:20:33 +0000

    Update css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -39,10 +39,8 @@ function App() {
       <Routes>
         <Route path="/" element={<EnterRoom socket={socket}/>} />
         <Route path="/room/:id" element={isMobile ? <Loading text="Aguarda que todos os jogadores entrem na sala..." page="story" url="game-started" socket={socket}/> : <WaitingRoom socket={socket}/>} />
-        {/*<Route path="/story/:id" element={isMobile ? <StoryGiver socket={socket}/> : <Loading text="Agora que cá estão todos, é hora de olhares para o teu telemóvel" socket={socket}/>} />*/}
         <Route path="/story/:id" element={<StoryGiver socket={socket}/>} />
         <Route path="/role/:id" element={<RoleGiver socket={socket}/>}/>
-        {/*<Route path="/role/:id" element={isMobile ? <RoleGiver socket={socket}/> : <Loading text="Agora que cá estão todos, é hora de olhares para o teu telemóvel" socket={socket}/>} />*/}
         <Route path="/drawing/:id" element={<DrawingPage socket={socket}/>} />
         <Route path="/arrange-frames/:id" element={<ArrangeFrames socket={socket}/>} />
         <Route path="/final/:id" element={isMobile ? <Loading text="Olha para o ecrã para principal para veres o resultado final" socket={socket}/> : <Final socket={socket}/>} />
